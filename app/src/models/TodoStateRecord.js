a6c3fd0deceb91d9ab1852611b8a1fb7e55bcb26
EricP96 <epastuer@outlook.com>
2018-01-29 02:24:39 -0700

    Removed unused members

--- app/src/models/TodoStateRecord.js
+++ app/src/models/TodoStateRecord.js
@@ -3,17 +3,13 @@ const { List, Map, Record } = require('immutable');
 const TodoRecord = require('./TodoRecord').default;
 
 const shape = {
-    groups: List(),
-    todos: List(),
-    groupTodos: Map()
+    todos: List()
 };
 
 class TodoStateRecord extends Record(shape) {
     static fromJS(todoState = {}) {
         return new TodoStateRecord({
-            groups: List(todoState.groups),
             todos: List(todoState.todos).map(TodoRecord.fromJS),
-            groupTodos: Map(todoState.groupTodos),
         });
     }
 }
